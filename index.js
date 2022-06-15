2e3c318ecc8466b3c61934b0d4c99165a0281871
Alejandro Grajales <alejoxdrums@gmail.com>
2022-06-15 17:49:38 -0500

    Functions

--- index.js
+++ index.js
@@ -37,14 +37,14 @@ function circleArea(radio) {
 
 //Square
 
-function calculateSquarePerimeter(){
+function calculateSquarePerimeter() {
     const input = document.getElementById('inputSquare');
     const value = input.value;
     const perimeter = squarePerimeter(value);
     alert(`The perimeter of the square is: ${perimeter} cm`);
 }
 
-function calculateSquareArea(){
+function calculateSquareArea() {
     const input = document.getElementById('inputSquare');
     const value = input.value;
     const area = squareArea(value);
@@ -53,7 +53,7 @@ function calculateSquareArea(){
 
 //Triangle
 
-function calculateTrianglePerimeter(){
+function calculateTrianglePerimeter() {
     const inputA = document.getElementById('inputTriangleA');
     const inputB = document.getElementById('inputTriangleB');
     const inputC = document.getElementById('inputTriangleC');
@@ -64,7 +64,7 @@ function calculateTrianglePerimeter(){
     alert(`The perimeter of the triangle is: ${perimeter} cm`);
 }
 
-function calculateTriangleArea(){
+function calculateTriangleArea() {
     const inputC = document.getElementById('inputTriangleC');
     const inputD = document.getElementById('inputTriangleD');
     const base = Number(inputC.value);
@@ -72,3 +72,51 @@ function calculateTriangleArea(){
     const area = triangleArea(base, height);
     alert(`The area of the triangle is: ${area} cm`);
 }
+
+// Isosceles triangle
+
+function isoscelesPerimeter(sides, base) {
+    return sides + sides + base;
+}
+
+function calculateIsoscelesPerimeter() {
+    const inputA = document.getElementById('inputTriangleE');
+    const inputB = document.getElementById('inputTriangleF');
+    const sides = Number(inputA.value);
+    const base = Number(inputB.value);
+    const perimeter = isoscelesPerimeter(sides, base);
+    alert(`The perimeter of the isosceles triangle is: ${perimeter} cm`);
+};
+
+function calculateIsoscelesHeight(sides, base) {
+    return (Math.sqrt((sides * sides) + (base * base)));
+};
+
+function calculateIsoscelesArea() {
+    const inputA = document.getElementById('inputTriangleE');
+    const inputB = document.getElementById('inputTriangleF');
+    const sides = Number(inputA.value);
+    const base = Number(inputB.value);
+    const height = calculateIsoscelesHeight(sides, base);
+    const area = triangleArea(base, height);
+    alert(`The height of triangle is: ${height}cm `);
+    alert(`The area of the isosceles triangle is: ${area} cm`);
+};
+
+// Circle
+
+function calculatedPerimeterCircle() {
+    const input = document.getElementById('radio');
+    const value = input.value;
+    const perimeter = circlePerimeter(value);
+    alert(`The perimeter of the circle is: ${perimeter}cm`)
+};
+
+function calculatedAreaCircle(){
+    const input = document.getElementById('radio');
+    const value = input.value;
+    const area = circleArea(value);
+    alert(`The area of the circle is: ${area}cm`)
+}
+
+
